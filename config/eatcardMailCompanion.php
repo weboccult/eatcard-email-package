1d252ec9f0f99f0f9ad800cdd39a9620a26d767e
wot-fenil <fenilk.wot2020@gmail.com>
2022-10-20 15:42:59 +0530

    set aws url in config file

--- config/eatcardMailCompanion.php
+++ config/eatcardMailCompanion.php
@@ -4,6 +4,6 @@
  * You can place your custom package configuration in here.
  */
 return [
-	'AWS_URL' => env('AWS_URL'),
+	'AWS_URL' => env('https://eatcard.s3.eu-central-1.amazonaws.com/'),
 	'CHAT_URL' => env('CHAT_URL')
 ];
