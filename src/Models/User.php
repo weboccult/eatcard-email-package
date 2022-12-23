e62cb38ba122c0ee92228133581dc0249c820193
Nainesh <nkrabadiya@gmail.com>
2022-12-23 17:47:29 +0100

    comment user boot method

--- src/Models/User.php
+++ src/Models/User.php
@@ -55,49 +55,49 @@ class User extends Authenticatable
 
     protected $appends = ['profile_image_url'];
 
-    protected static function boot()
-        {
-            parent::boot();
-
-            static::created(function ($model){
-                Log::info(' User added |  User : ' . json_encode($model) . ' | Added by : ' . auth()->user()->name);
-            });
-
-            static::updated(function($model)
-            {
-                $dirty = $model->getDirty();
-                if (!empty($dirty)) {
-	                $user=getUserDetail();
-                    foreach ($dirty as $field => $newdata) {
-                        $olddata = $model->getOriginal($field);
-                        $oldData['old'][$field] = $olddata;
-                        $newData['new'][$field] = $newdata;
-                    }
-
-                    $user_role = User::find($model->id)->getUserRoleAttribute();
-                    if ($user_role == 'store_owner') {
-                        $role = 'Store Owner';
-                    } else if ($user_role == 'store_manager') {
-                        $role = 'Store Manager';
-                    } else {
-                        $role = ' ';
-                    }
-                    if(auth()->user()) {
-    	                $msg = ' ' . $role . ' Updated: ' . $user['role'] . ' (' . $user['name'] . '-' . $user['id'] . ') updated a ' . $role . ' detail ' . $model->name . ' | id-' . $model->id . ' . ' . print_r($oldData, true) . ' => ' . print_r($newData, true) . '' . ' | User '. json_encode( auth()->user()) . '' ;
-    	                Log::info($msg. ', IP address : '.request()->ip(). ', browser : '. request()->header('User-Agent'));
-
-                    }
-                }
-            });
-
-            static::deleted(function ($model){
-                $dirty_old_data = $model->getOriginal();
-                if (!empty($dirty_old_data)) {
-                   Log::info($model->name .' Deleted by : '. auth()->user()->name . '  Deleted data  | ' . print_r($dirty_old_data , true));
-                }
-            });
-
-        }
+//    protected static function boot()
+//        {
+//            parent::boot();
+//
+//            static::created(function ($model){
+//                Log::info(' User added |  User : ' . json_encode($model) . ' | Added by : ' . auth()->user()->name);
+//            });
+//
+//            static::updated(function($model)
+//            {
+//                $dirty = $model->getDirty();
+//                if (!empty($dirty)) {
+//	                $user=getUserDetail();
+//                    foreach ($dirty as $field => $newdata) {
+//                        $olddata = $model->getOriginal($field);
+//                        $oldData['old'][$field] = $olddata;
+//                        $newData['new'][$field] = $newdata;
+//                    }
+//
+//                    $user_role = User::find($model->id)->getUserRoleAttribute();
+//                    if ($user_role == 'store_owner') {
+//                        $role = 'Store Owner';
+//                    } else if ($user_role == 'store_manager') {
+//                        $role = 'Store Manager';
+//                    } else {
+//                        $role = ' ';
+//                    }
+//                    if(auth()->user()) {
+//    	                $msg = ' ' . $role . ' Updated: ' . $user['role'] . ' (' . $user['name'] . '-' . $user['id'] . ') updated a ' . $role . ' detail ' . $model->name . ' | id-' . $model->id . ' . ' . print_r($oldData, true) . ' => ' . print_r($newData, true) . '' . ' | User '. json_encode( auth()->user()) . '' ;
+//    	                Log::info($msg. ', IP address : '.request()->ip(). ', browser : '. request()->header('User-Agent'));
+//
+//                    }
+//                }
+//            });
+//
+//            static::deleted(function ($model){
+//                $dirty_old_data = $model->getOriginal();
+//                if (!empty($dirty_old_data)) {
+//                   Log::info($model->name .' Deleted by : '. auth()->user()->name . '  Deleted data  | ' . print_r($dirty_old_data , true));
+//                }
+//            });
+//
+//        }
 
 
     public function getProfileImageUrlAttribute()
